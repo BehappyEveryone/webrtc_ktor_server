f248fc7698dee0d1ddffca5d90cb354a78fb4481
yeonjun <kgbduswns@gmail.com>
2024-04-06 05:51:00 +0900

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,56 @@
+import io.ktor.application.*
+import io.ktor.http.cio.websocket.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.channels.ClosedReceiveChannelException
+import java.time.Duration
+import java.util.*
+
+/**
+ * Originally written by Artem Bagritsevich.
+ *
+ * https://github.com/artem-bagritsevich/WebRTCKtorSignalingServerExample
+ */
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@JvmOverloads
+fun Application.module(testing: Boolean = false) {
+
+    install(WebSockets) {
+        pingPeriod = Duration.ofSeconds(15)
+        timeout = Duration.ofSeconds(15)
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+    }
+
+    routing {
+
+        get("/") {
+            call.respond("Hello from WebRTC signaling server")
+        }
+        webSocket("/rtc") {
+            val sessionID = UUID.randomUUID()
+            try {
+                SessionManager.onSessionStarted(sessionID, this)
+
+                while (true) {
+                    when(val frame = incoming.receive()){
+                        is Frame.Text -> {
+                            SessionManager.onMessage(sessionID, frame.readText())
+                        }
+                        else -> Unit
+                    }
+                }
+            } catch (e: ClosedReceiveChannelException) {
+                println("onClose $sessionID")
+                SessionManager.onSessionClose(sessionID)
+            } catch (e: Throwable) {
+                println("onError $sessionID $e")
+                SessionManager.onSessionClose(sessionID)
+            }
+        }
+    }
+}
+
